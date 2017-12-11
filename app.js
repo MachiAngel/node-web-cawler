d0ba863a99f9063faa235eef1c34444fe0873091
Shiang-Yu Huang <sky90456@hotmail.com>
2017-12-11 14:27:41 +0800

    1. fix require bug

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const schedule = require('node-schedule')
 //const {saveTaiwankBankHistory} = require('./util/util')
 const {refreshTaiwanBankData} = require('./util/util')
 const {refreshMegaBankData} = require('./util/util')
+const {refreshEsunBankData} = require('./util/util')
+const {refreshSinopacBankData} = require('./util/util')
 
 // const rule = new schedule.RecurrenceRule()
 // rule.minute = [0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60]
